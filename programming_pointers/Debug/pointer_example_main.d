58c65389348f7e37168186a462a136d644c9a022
Daniel <daniellomis@yahoo.com>
2024-09-29 22:06:13 -0400

    Modified files to work on MCU

---
+++ programming_pointers/Debug/pointer_example_main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+pointer_example_main.obj: ../pointer_example_main.c
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+pointer_example_main.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+
+../pointer_example_main.c:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
---
+++ programming_structs/Debug/ccs/startup_msp432p401r_ccs.d
@@ -0,0 +1,36 @@
+# FIXED
+
+ccs/startup_msp432p401r_ccs.obj: ../ccs/startup_msp432p401r_ccs.c
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+ccs/startup_msp432p401r_ccs.obj: C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+
+../ccs/startup_msp432p401r_ccs.c:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/Programming\ Software/ti/ccs1280/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
